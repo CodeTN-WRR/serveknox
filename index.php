3f092d06ab6c19bc6600c8740a62ed42e3b93f4a
Ayden Case <aydengc@gmail.com>
2016-11-17 21:39:37 +0000

    Slim

--- index.php
+++ index.php
@@ -5,28 +5,37 @@
     use \Psr\Http\Message\ResponseInterface as Response;
     
     require 'vendor/autoload.php';
-
-    // Main Instance
+    
     $app = new \Slim\App(array(
     'debug' => true));
     
+    $container = $app->getContainer();
+
+    $container['view'] = function ($container) {
+        $view = new \Slim\Views\Twig('views', [
+            'cache' => 'false'
+        ]);
+        
+        // Instantiate and add Slim specific extension
+        $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+        $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+    
+        return $view;
+    };
+    
     $app->get('/', function ($request, $response) {
-       return $this->view->render($response, 'example.html');
-    })->setName('home');
+        return $this->view->render($response, 'example.twig');
+    })->setName('profile');
     
     
-    //Will Remove At Production
     
-    $configuration = [
-    'settings' => [
-        'displayErrorDetails' => true,
-        ],
-    ];
-    $c = new \Slim\Container($configuration);
-    $app = new \Slim\App($c);
     
-    //End of Production Removal
+    $app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'profile.html', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
     
-    //Run App
     $app->run();
+
 ?>
