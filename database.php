1a88f48daaf5c8e2fbd583105dc0164628441590
Ayden Case <aydengc@gmail.com>
2016-11-17 21:53:59 +0000

    Update

--- database.php
+++ database.php
@@ -19,17 +19,5 @@
 		echo "</div>";
 		die();
 	}
-	
-	try {
-		$results = $db->query("SELECT items.name, items.id, description, price, categories.name AS category, image FROM items JOIN categories ON items.category_id = categories.id ORDER BY price DESC;");
-		$items = $results->fetchAll(PDO::FETCH_ASSOC);
-	} catch (Exception $e) {
-		echo "<h1 style='text-align: center; color: red; text-decoration: underline'>";
-		echo "Failed Query.";
-		echo "</h1>";
-		echo "<div style='margin: auto; max-width: 40%; border: 2px solid black; background-color: #b30000; color: white; border-radius: 5px'>";
-		echo "</div>";
-		die();
-	}
 
 ?>
