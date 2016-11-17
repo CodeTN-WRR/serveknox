83214a322213722d5b6f8b9074f18fbe157dddcb
Rob Scott <rob@scottclan.cc>
2016-11-17 21:31:28 +0000

    Comments

--- mysql/create.sql
+++ mysql/create.sql
@@ -1,9 +1,13 @@
+-- Delete any existing database
 drop database if exists testdb;
 
+-- Create a new database
 create database testdb;
 
+-- Switch to it so we can create its tables
 use testdb;
 
+-- Create tables
 create table user (
     firstname VARCHAR(64),
     lastname VARCHAR(64),
